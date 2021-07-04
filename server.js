963222988ef0919b006f426f419da68cc4c0487a
ni3mumbaikar <nitinkale035@gmail.com>
2021-07-04 11:30:05 +0530

    added babel using yarn

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname,'./dist')))
+app.use(express.static(path.join(__dirname, "./dist")));
 
-app.get("/",(req,res)=>{
-    res.status(200).sendFile(__dirname + '/dist/app.html')
-})
-
-app.get("/test",(req,res)=>{
-    res.status(200).send("Welcome to test page")
-})
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is listening at : ${process.env.PORT}`)
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/dist/app.html");
 });
 
+app.get("/test", (req, res) => {
+  res.status(200).send("Welcome to test page");
+});
 
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening at : ${process.env.PORT}`);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-const path = require('path')
+const path = require("path");
 
 const config = {
-        entry : path.resolve(__dirname,'./src/index.js'),
-        mode: "development",
-    output :{
-        path: path.resolve(__dirname,'./dist'),
-        filename: 'app.bundle.js'
-    },
-    devtool : 'source-map'
-}
+  entry: path.resolve(__dirname, "./src/index.js"),
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "app.bundle.js",
+  },
 
-module.exports =  config;
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+
+  devtool: "source-map",
+};
+
+module.exports = config;
